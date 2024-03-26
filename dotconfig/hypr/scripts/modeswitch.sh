4b69daf69a2557120b6c31600d7710889957143b
Liam Rahum <liam@rahum.co.il>
2024-03-26 18:55:40 +0200

    Mode change optimizations

--- dotconfig/hypr/scripts/modeswitch.sh
+++ dotconfig/hypr/scripts/modeswitch.sh
@@ -27,19 +27,6 @@ else
     exit 1
 fi
 
-### hyprland ###
-ln -fs $CFGDIR/hypr/${S_MODE}.conf $CFGDIR/hypr/theme.conf
-
-### swwwallpaper ###
-x=`echo $S_MODE | cut -c 1`
-bash $CFGDIR/swww/swwwallpaper.sh -$x
-
-### qt5ct ###
-ln -fs $CFGDIR/qt5ct/colors/${S_MODE}.conf $CFGDIR/qt5ct/colors/theme.conf
-
-### rofi ###
-ln -fs $CFGDIR/rofi/${S_MODE}.rasi $CFGDIR/rofi/theme.rasi
-
 ### Firefox/Librewolf ###
 ln -fs $CFGDIR/nwg-look/Firefox-Theme-Colors/${S_MODE}.css $CFGDIR/nwg-look/Firefox-Theme-Colors/theme.css
 
@@ -47,8 +34,23 @@ ln -fs $CFGDIR/nwg-look/Firefox-Theme-Colors/${S_MODE}.css $CFGDIR/nwg-look/Fire
 ln -fs $CFGDIR/kitty/themes/${S_MODE}.conf $CFGDIR/kitty/themes/theme.conf
 killall -SIGUSR1 kitty
 
+### hyprland ###
+ln -fs $CFGDIR/hypr/${S_MODE}.conf $CFGDIR/hypr/theme.conf
+
+### qt5ct ###
+ln -fs $CFGDIR/qt5ct/colors/${S_MODE}.conf $CFGDIR/qt5ct/colors/theme.conf
+
+### rofi ###
+ln -fs $CFGDIR/rofi/${S_MODE}.rasi $CFGDIR/rofi/theme.rasi
+
 ### waybar ###
 ln -fs $CFGDIR/waybar/${S_MODE}.css $CFGDIR/waybar/style.css
-sleep 1
-killall -SIGUSR2 waybar
-nohup waybar &
+
+### swwwallpaper ###
+x=`echo $S_MODE | cut -c 1`
+bash $CFGDIR/swww/swwwallpaper.sh -$x
+
+#sleep 1
+#killall -SIGUSR2 waybar
+#nohup waybar &
+
